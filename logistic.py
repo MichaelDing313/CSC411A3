46d1b21a2ffec2d8d91c49a64e21cb954e0788dc
shiyudian <shi0118@hotmail.com>
2017-03-19 02:53:00 -0400

    Update logistic.py

--- logistic.py
+++ logistic.py
@@ -5,6 +5,7 @@ from collections import OrderedDict
 import re
 from collections import Counter
 
+import operator
 
 # read polarity dataset
 def read_file():
@@ -38,12 +39,20 @@ def read_file():
             neg_dictionary = store_word
             
     
-    return list(OrderedDict.fromkeys(keyword))
+    return list(OrderedDict.fromkeys(keyword)),pos_dictionary,neg_dictionary
     
-    
-keyword = read_file()
-
-#def max_word(directory):
+##    
+keyword,pos_dictionary,neg_dictionary = read_file()
+# find the top 3 words
+top_pos = []
+top_neg = []
+for i in range (3):
+    top_pos.append(max(pos_dictionary,key=pos_dictionary.get))
+    pos_dictionary[top_pos[i]] = 0
+    top_neg.append(max(neg_dictionary,key=neg_dictionary.get))
+    neg_dictionary[top_neg[i]] = 0
+top_pos
+top_neg
     
     
     
